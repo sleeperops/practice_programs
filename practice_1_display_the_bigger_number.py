8f2b3441fb1517415a0f391d4d60f21f972c1732
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-13 21:31:58 +0800

    Added error handling
    Added a try and except section. This will ensure that the inputs will only be integers to avoid a type error.

--- practice_1_display_the_bigger_number.py
+++ practice_1_display_the_bigger_number.py
@@ -8,6 +8,13 @@
 input_a = input('Input the first number: ')
 input_b = input('Input the second number: ')
 
+try:
+    input_a = int(input_a)
+    input_b = int(input_b)
+except:
+    print('>>Not numbers')
+# ==============================================
+
 def greater_number(a, b): 
     if a > b:
         return str(a)
