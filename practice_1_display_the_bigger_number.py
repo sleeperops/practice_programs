61a70d2e0362963735de561e8491973d1637b111
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-13 21:26:17 +0800

    Added the ability to ask for input

--- practice_1_display_the_bigger_number.py
+++ practice_1_display_the_bigger_number.py
@@ -4,3 +4,7 @@
 # if the first number is bigger than the second, print the first number
 # else, if the second number is bigger, print the second number
 # otherwise, print 'equal'
+
+input_a = input('Input the first number: ')
+input_b = input('Input the second number: ')
+
