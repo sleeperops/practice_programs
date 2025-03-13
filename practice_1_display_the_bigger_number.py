d87c47113982f3a52f6b17c2c1301a49823f3035
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-13 21:24:22 +0800

    added the pseudocode for the first program

---
+++ practice_1_display_the_bigger_number.py
@@ -0,0 +1,6 @@
+# ask user to input first number
+# asks user to input second number
+
+# if the first number is bigger than the second, print the first number
+# else, if the second number is bigger, print the second number
+# otherwise, print 'equal'
