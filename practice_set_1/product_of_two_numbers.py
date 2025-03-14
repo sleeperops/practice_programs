45f47e9e95ce353f8a6e7be77639985e65d81932
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 17:56:32 +0800

    Added error handling

--- practice_set_1/product_of_two_numbers.py
+++ practice_set_1/product_of_two_numbers.py
@@ -4,3 +4,21 @@
 
 # multiply the first input to the second input
 # # display the product
+
+input_a = input('Input the first number: ')
+input_b = input('Input the second number: ')
+
+try:
+    input_a = int(input_a)
+    input_b = int(input_b)
+except:
+    print('>>Not numbers')
+
+
+def product_ab(a, b):
+    return a * b
+
+print(f'Answer: {product_ab(input_a, input_b)}')
+
+
+
