a84cd6f1898da009efe954c880ac38da4faaa4a0
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 19:52:55 +0800

    Added function that subtracts the two inputs

--- practice_set_2/difference_of_two_numbers.py
+++ practice_set_2/difference_of_two_numbers.py
@@ -3,3 +3,8 @@
 # enter second input
 # subtract the second number from the first
 # print output
+
+first_input = input('Enter the first number: ')
+second_input = input('Enter the second number: ')
+def difference(input1, input2):
+    return input1 - input2
