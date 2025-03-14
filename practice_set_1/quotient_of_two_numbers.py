7fd4a40e8ac120337957d717c91bc003ac4642af
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:08:34 +0800

    Used the format of previous programs
    Used the previous error handling as well as format. For here, a divider operator is used to divide the inputs. When the second number or the denominator is zero, the input is undefined

--- practice_set_1/quotient_of_two_numbers.py
+++ practice_set_1/quotient_of_two_numbers.py
@@ -6,5 +6,23 @@
 # display the quotient
 
 
+input_a = input('Input the first number: ')
+input_b = input('Input the second number: ')
+
+def quotient_ab(a, b):
+    if a == 0 or b == 0:
+        return 'Undefined'
+    else:
+        return a / b
+try:
+    input_a = int(input_a)
+    input_b = int(input_b)
+    print(f'Answer: {quotient_ab(input_a, input_b)}')
+except:
+    print('>>Not numbers')
+# ==============================================
+
+
+
 
 
