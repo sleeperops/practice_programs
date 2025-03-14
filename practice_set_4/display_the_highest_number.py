b8fe6fc027ecc855cb2a4e8431155da9dd6bb523
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 23:39:25 +0800

    Added pseudocode

---
+++ practice_set_4/display_the_highest_number.py
@@ -0,0 +1,9 @@
+# displays the highest number
+# initialize a list called input_list
+# run a while loop as long as it is true
+# for every loop, ask the user for an input
+# try if the input can be converted to an integer
+# > if so add the number to the input_numbers list
+# else if it is not an integer print 'not a number' and break from the loop
+# sort the input_numbers list using sort() function
+# print the [-1] index or the biggest one
