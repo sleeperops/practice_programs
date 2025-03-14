ab08590217bb176c4059b50623831e77576df917
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 23:45:58 +0800

    Added pseudocode

---
+++ practice_set_4/highest_to_lowest.py
@@ -0,0 +1,7 @@
+# initialize a list called input_list
+# run a while loop as long as it is true
+# for every loop, ask the user for an input
+# try if the input can be converted to an integer
+# > if so add the number to the input_numbers list
+# else if it is not an integer print 'not a number' and break from the loop
+# sort the input_numbers list using sort(reverse = True) method to reverse the sorting
