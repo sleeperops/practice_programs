9dc25d203d50104df3780d01bc7d55d24e29493e
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 19:57:14 +0800

    Added function that allows floor division of two numbers

--- practice_set_2/division_of_two_numbers_no_decimal.py
+++ practice_set_2/division_of_two_numbers_no_decimal.py
@@ -3,3 +3,9 @@
 # enter second input
 # divide the first input by the second using floor division
 # display the output
+
+first_input = input('Enter the first number: ')
+second_input = input('Enter the second number: ')
+
+def quotient_no_dec(input1, input2):
+    return f'{input1//input2}'
