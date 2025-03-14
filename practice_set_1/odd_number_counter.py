74ad2f8fd4b505e8cf070b75b8852837573020d2
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:35:40 +0800

    Added pseudocode

---
+++ practice_set_1/odd_number_counter.py
@@ -0,0 +1,4 @@
+# use a for loop to ask for a number ten times
+# divide the number by two
+# if it returns a remainder of one (the number is odd), increment the count of odd numbers by 1
+# print the count of odd numbers
