a04dd3cf2f213df3817a9848a4e4766de9e5cd83
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 21:11:23 +0800

    Added pseudocode

---
+++ practice_set_2/numbers_between_two.py
@@ -0,0 +1,10 @@
+# prints all numbers in between two numbers
+
+# enter the first input
+# enter the second input
+# use try and except to make sure the outputs are integers
+# if first input is smaller, make it as the minimum value(floor), and make the second input the maximum value (ceiling)
+# if second input is smaller, make it as the minimum value(floor), and make the first input the maximum value (ceiling)
+# else make it equal
+# run a for loop from the minimum(floor) value up to the maximum value(ceiling)
+# print the numbers in between
