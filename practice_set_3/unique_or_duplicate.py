0d525641ca30c36669ea8c3ee201e0e35aed8071
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 22:41:52 +0800

    Added pseudocode

---
+++ practice_set_3/unique_or_duplicate.py
@@ -0,0 +1,10 @@
+# checks if the number is unique or a duplicate
+# initialize a list called input_list
+# run a while loop as long as it true
+# for every loop, ask the user for an input
+# try if the input can be converted to an integer
+# > if so, check if the number is already in the number list
+# > if it is, print 'duplicate', else, print 'unique'
+# > add the number to the input_numbers list
+# else if it is not an integer print 'not a number' and break from the loop
+
