4d95bc6bff1d97b27b932e679d82a12c1b4da4b7
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 13:51:12 +0800

    Created file

---
+++ practice_2_checks_if_two_numbers_are_equal.py
@@ -0,0 +1,7 @@
+# ask user for the first input and store it in input_a
+# ask the user for the second input and store it in input_b
+# check if both inputs can be converted to an integer
+# if not, display 'not numbers'
+# If the two values are equal, display 'equal'
+# else, display 'not equal'
+
