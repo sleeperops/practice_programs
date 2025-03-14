5f800d83023631bee867fc6d2cec998704c2d68b
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 20:53:21 +0800

    Added function that prints all odd numbers from 0-100
    The program counts up to a hundred. If it detects a number that isn't divisible by 2 (odd), it will display the number.

--- practice_set_2/display_all_odd_numbers_from_zero_to_a_hundred.py
+++ practice_set_2/display_all_odd_numbers_from_zero_to_a_hundred.py
@@ -4,3 +4,12 @@
 # while counter is not equal to a hundred
 # increment it by 1
 # if the current number(counter) is not divisible by 2 (is odd), print it
+
+
+counter = 0
+while counter != 100:
+    counter += 1
+    if counter % 2 == 1:
+        print(counter, end = ' ')
+    
+    
