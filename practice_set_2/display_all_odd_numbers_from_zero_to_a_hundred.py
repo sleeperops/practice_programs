08869a2a228f36d53d913d98729809d8f28258e9
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 20:51:39 +0800

    Added pseudocode

---
+++ practice_set_2/display_all_odd_numbers_from_zero_to_a_hundred.py
@@ -0,0 +1,6 @@
+# prints all odd numbers from 0 - 100 using while loop
+
+# initialize counter
+# while counter is not equal to a hundred
+# increment it by 1
+# if the current number(counter) is not divisible by 2 (is odd), print it
