d9a43498023b4f0535479e5c99e48d156d1d5266
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:30:26 +0800

    Added a countdown feature
    This feature allows the user to see how many numbers are left to input. It starts from ten which is the total number of loops, then for every loop, it decrements by one unit.

--- practice_set_1/summation_of_ten_numbers.py
+++ practice_set_1/summation_of_ten_numbers.py
@@ -5,9 +5,11 @@
 
 def summate():
     total = 0
+    counter = 10  # countdown, for auxillary purposes
     for _ in range(10):
-        number = int(input(f'Input a number: '))
+        number = int(input(f'Input a number: ({counter}) '))
         total += number
+        counter -= 1
     print(f'The sum of all the numbers is: {total}')
     
 summate()
