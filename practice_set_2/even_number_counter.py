e5966c0106cb54e7e5e31ff3e51daa78a78d0a2e
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 20:46:28 +0800

    Added pseudocode

---
+++ practice_set_2/even_number_counter.py
@@ -0,0 +1,7 @@
+# counts the amount of even numbers based on user input
+
+# initialize a variable named 'total'
+# use a for loop to loop ten times
+# for each loop, ask for an input
+# if the input is divisible by two (input is even), increment total by 1
+# print total
