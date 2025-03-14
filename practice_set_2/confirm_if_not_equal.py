01877171dc94bfd6430f388d288421553bdf60bc
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 19:47:48 +0800

    Added function that prints not equal when numbers are not equal

--- practice_set_2/confirm_if_not_equal.py
+++ practice_set_2/confirm_if_not_equal.py
@@ -4,3 +4,12 @@
 # if first input is not equal to second
 # print 'not a number'
 # else print 'equal'
+
+first_input = input('Enter the first number: ')
+second_input = input('Enter the second number: ')
+
+def not_equal(input_1, input_2):
+    if input_1 != input_2:
+        return 'Not Equal'
+    else:
+        return 'Equal'
