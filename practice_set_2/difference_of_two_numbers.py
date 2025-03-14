cd9bf19ffbf00ec56a9fe77d46816c0c8166c67a
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 19:53:52 +0800

    Added an error handler
    Ensures that input will only  be an integer

--- practice_set_2/difference_of_two_numbers.py
+++ practice_set_2/difference_of_two_numbers.py
@@ -8,3 +8,11 @@ first_input = input('Enter the first number: ')
 second_input = input('Enter the second number: ')
 def difference(input1, input2):
     return input1 - input2
+try:
+    first_input = int(first_input)
+    second_input = int(second_input)
+    print(f'Difference: {difference(first_input, second_input)}')
+except:
+    print('>>Not a number')
+ # ===============================================
+ 
