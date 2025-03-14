0d0d92873c96e00b6e0f2ed7c6a2be19d24f3a8e
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 20:59:21 +0800

    Added the function that prints only numbers not divisible by 5 or 10
    Used modulo operator

--- practice_set_2/display_numbers_not_divisible_by_10_or_5.py
+++ practice_set_2/display_numbers_not_divisible_by_10_or_5.py
@@ -4,3 +4,9 @@
 # for every loop, check if the current number is divisible by 5 or 10
 # if it is, skip that number
 # else, print it
+
+for num in range(100):
+    if num % 5 == 0 or num % 10 == 0:
+        continue
+    else:
+        print(num, end = ' ')
