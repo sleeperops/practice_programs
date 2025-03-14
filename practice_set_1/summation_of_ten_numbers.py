3f28f2026d9ca4bf25b6e1fb3c8e5c14c7f70ec6
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:27:29 +0800

    Added the summation function
    Uses a for loop to ask input from the user. The answers are then accumulated in the variable named 'total'.

--- practice_set_1/summation_of_ten_numbers.py
+++ practice_set_1/summation_of_ten_numbers.py
@@ -2,3 +2,12 @@
 # use a for loop to ask for a number ten times
 # for every iteration, add the input to the total
 # once ten iterations have completed, print the total
+
+def summate():
+    total = 0
+    for _ in range(10):
+        number = int(input(f'Input a number: '))
+        total += number
+    print(f'The sum of all the numbers is: {total}')
+    
+summate()
