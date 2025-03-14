f8ae05df1c43f859245781d0ecf5716f286a5a0c
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 17:54:30 +0800

    added the pseudocode

---
+++ practice_set_1/product_of_two_numbers.py
@@ -0,0 +1,6 @@
+# accept input for first input
+# accept input for second input
+# check if input is an integer, if not, display 'Not a number'
+
+# multiply the first input to the second input
+# # display the product
