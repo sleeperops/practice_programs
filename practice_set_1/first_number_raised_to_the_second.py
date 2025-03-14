78126f0a3b692b18d4c3f579f7a2a99f0dcdbfa6
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:12:20 +0800

    Used the format of previous programs
    For this program, an exponenet ('**') operator was used to raise the first number to the second

--- practice_set_1/first_number_raised_to_the_second.py
+++ practice_set_1/first_number_raised_to_the_second.py
@@ -5,3 +5,17 @@
 # raise the first input to the second input
 # # display the answer
 
+def exponent_ab(a, b):
+    return a ** b
+
+
+input_a = input('Input the first number: ')
+input_b = input('Input the second number: ')
+try:
+    input_a = int(input_a)
+    input_b = int(input_b)
+    print(f'Answer: {exponent_ab(input_a, input_b)}')
+
+except:
+    print('>>Not numbers')
+# ==============================================
