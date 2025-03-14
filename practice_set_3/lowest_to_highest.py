2145e9c86f984525b7ae46f9c445481b7b1ceb95
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 23:03:20 +0800

    Added pseudocode

---
+++ practice_set_3/lowest_to_highest.py
@@ -0,0 +1,10 @@
+# sorts a list of numbers
+# initialize a list called input_list
+# run a while loop as long as it is true
+# for every loop, ask the user for an input
+# try if the input can be converted to an integer
+# > if so add the number to the input_numbers list
+# else if it is not an integer print 'not a number' and break from the loop
+# sort the input_numbers list using sort() method
+# print the list
+
