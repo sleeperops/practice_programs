2760d0ebab46d40edd5678eec7b6906665d9661f
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:45:19 +0800

    Added pseudocode

---
+++ practice_set_1/even_numbers_displayer.py
@@ -0,0 +1,5 @@
+# use a for loop to iterate 101 times (for loop is excludes the last number)
+# for every loop, use modulo to check if number is divisible by two
+# if yes, print
+# else, continue the loop
+
