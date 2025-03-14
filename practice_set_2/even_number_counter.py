83a622a91fd0f50d7ea6b9cfda3130046fee4221
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 20:48:06 +0800

    Added the function that counts the even numbers. Added countdown as well
    The function will ask for ten numbers. It will then check if those numbers are divisible by two, if it is, it will be counted.

--- practice_set_2/even_number_counter.py
+++ practice_set_2/even_number_counter.py
@@ -5,3 +5,18 @@
 # for each loop, ask for an input
 # if the input is divisible by two (input is even), increment total by 1
 # print total
+
+
+def even_number_count():
+    total_even = 0
+    counter = 10  # countdown, for auxillary purposes
+    for _ in range(10):
+        number = int(input(f'Input a number ({counter}): '))
+        if number % 2 == 0:
+            total_even += 1      
+        
+        counter -= 1 # countdown, for auxillary purposes
+    return total_even
+
+
+print(f'Total number of even numbers: {even_number_count()}')
