a48ee4f668fc3cba4582de0dd7d3fc3e937b898d
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 22:06:14 +0800

    Added pseudocode

---
+++ practice_set_3/display_only_unduplicating.py
@@ -0,0 +1,12 @@
+# use a for loop to ask for ten numbers
+# initialize a list named 'input_numbers' that will store all of the inputs
+# initialize an empty list named 'unduplicating_list'
+# initialize an empty list named 'marked_as_duplicate'
+# run a for loop in the 'input_numbers' list
+# for every loop check if the numbers in that list is in the unduplicating list
+# if not, add it
+# elif its already in the list, add the number to marked_as_duplicate
+# run another for loop in unduplicating_list
+# for every loop, check if the item is in marked_as_duplicate
+# if it is, remove the item from the list
+# print the unduplicating list
