456e835c3a6c294f95a4345de307e8ab3e055c91
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:55:01 +0800

    Rearra

--- display_the_bigger_number.py
+++ display_the_bigger_number.py
@@ -8,13 +8,6 @@
 input_a = input('Input the first number: ')
 input_b = input('Input the second number: ')
 
-try:
-    input_a = int(input_a)
-    input_b = int(input_b)
-except:
-    print('>>Not numbers')
-# ==============================================
-
 def greater_number(a, b): 
     if a > b:
         return str(a)
@@ -22,5 +15,14 @@ def greater_number(a, b):
         return 'Equal'
     else:
         return str(b)
+try:
+    input_a = int(input_a)
+    input_b = int(input_b)
+    print(f'Answer: {greater_number(input_a, input_b)}')
+except:
+    print('>>Not numbers')
+# ==============================================
 
-print(f'Answer: {greater_number(input_a, input_b)}')
+
+
+
