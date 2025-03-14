621b8ca4b342f544452ff960ee66988d6a551394
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 17:49:56 +0800

    Added pseudocode

---
+++ practice_set_1/sum_of_two_numbers.py
@@ -0,0 +1,6 @@
+# accept input for first input
+# accept input for second input
+# check if input is an integer, if not, display 'Not a number'
+
+# add the first input to the second input
+# # display the sum
