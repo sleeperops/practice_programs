cb80cfcb02b7e46924a05df3bf34bbedae61cac0
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 23:47:32 +0800

    Accepts and sorts but the sort function prints from highest to lowest
    reverses by using sort(reverse = True)

--- practice_set_4/highest_to_lowest.py
+++ practice_set_4/highest_to_lowest.py
@@ -5,3 +5,15 @@
 # > if so add the number to the input_numbers list
 # else if it is not an integer print 'not a number' and break from the loop
 # sort the input_numbers list using sort(reverse = True) method to reverse the sorting
+
+input_numbers = []
+while True:
+    try:
+        user_input = int(input(f'Enter a number: '))
+        input_numbers.append(user_input)
+    
+    except:
+        print('>>Input is not a number! ')
+        break
+input_numbers.sort(reverse = True)
+print(input_numbers)
