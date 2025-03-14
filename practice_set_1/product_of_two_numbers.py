ecab95626b01e1b4a4f0f5ab08792afab75824b8
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:00:59 +0800

    Rearranged print statement to be inside the try clause
    Previous arrangement caused the except section to run and still throw an error. I've realized it was because the print statement is outside the try cause, which causes it to run regardless of the result of the try and except statement.

--- practice_set_1/product_of_two_numbers.py
+++ practice_set_1/product_of_two_numbers.py
@@ -8,17 +8,17 @@
 input_a = input('Input the first number: ')
 input_b = input('Input the second number: ')
 
+def product_ab(a, b):
+    return a * b
+
 try:
     input_a = int(input_a)
     input_b = int(input_b)
+    print(f'Answer: {product_ab(input_a, input_b)}')
 except:
     print('>>Not numbers')
 
 
-def product_ab(a, b):
-    return a * b
-
-print(f'Answer: {product_ab(input_a, input_b)}')
 
 
 
