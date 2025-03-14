a96c0e2c54997f5215ad4da5b735390847364e7a
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:10:38 +0800

    Added pseudocode

---
+++ practice_set_1/first_number_raised_to_the_second.py
@@ -0,0 +1,7 @@
+# accept input for first input
+# accept input for second input
+# check if input is an integer, if not, display 'Not a number'
+
+# raise the first input to the second input
+# # display the answer
+
