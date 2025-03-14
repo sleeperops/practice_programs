83612823d7aaec71fb8d8522a9bf4fde00659623
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 20:41:07 +0800

    Added countdown
    The countdown allows the user to see how many numbers are still needed to be entered.

--- practice_set_2/first_number_minus_nine_other_numbers.py
+++ practice_set_2/first_number_minus_nine_other_numbers.py
@@ -8,10 +8,12 @@
 
 def difference_ten_numbers():
     total = 0
+    counter = 9  # countdown, for auxillary purposes
     first_number = int(input('Input the first number as the minuend (10): ')) # to divide the other numbers to
     for _ in range(9):
-        number = int(input(f'Input the rest as a subtrahend : '))
+        number = int(input(f'Input the rest as a subtrahend ({counter}): '))
         total += number
+        counter -= 1 # countdown, for auxillary purposes
     return first_number - total
     
 print(f'The first number minus the rest: {difference_ten_numbers()}')
