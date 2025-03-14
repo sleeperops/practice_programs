30bb9a9fe4efe8cb0a3e149876ab7e92be057496
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:36:44 +0800

    Added the ability to ask for input
    Uses a for loop to ask for an input ten times

--- practice_set_1/odd_number_counter.py
+++ practice_set_1/odd_number_counter.py
@@ -2,3 +2,10 @@
 # divide the number by two
 # if it returns a remainder of one (the number is odd), increment the count of odd numbers by 1
 # print the count of odd numbers
+
+def odd_numbers():
+    odd_num_count = 0
+    counter = 10  # countdown, for auxillary purposes
+    for _ in range(10):  # Asks for a number ------------------------
+        number = int(input(f'Input a number ({counter}): '))
+      
