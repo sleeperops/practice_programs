c7167a466caa8c5274740f0834dc5ac50d3380b1
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:38:13 +0800

    Used modulo operator to distinguish odd from even

--- practice_set_1/odd_number_counter.py
+++ practice_set_1/odd_number_counter.py
@@ -5,7 +5,11 @@
 
 def odd_numbers():
     odd_num_count = 0
-    counter = 10  # countdown, for auxillary purposes
     for _ in range(10):  # Asks for a number ------------------------
-        number = int(input(f'Input a number ({counter}): '))
+        number = int(input(f'Input a number: '))
+        if number % 2 == 1:  # if odd, add 1 to the total amount of odd numbers and continue
+            odd_num_count += 1
+    print(f' There are {odd_num_count} odd numbers')
+
+odd_numbers()
       
