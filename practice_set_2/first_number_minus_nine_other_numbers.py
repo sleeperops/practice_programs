1151e0e705986b5d29d7f41f57fb8022ffc7623c
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 20:38:17 +0800

    Added the function
    The first input is outside the for loop to make it easier to subtract

--- practice_set_2/first_number_minus_nine_other_numbers.py
+++ practice_set_2/first_number_minus_nine_other_numbers.py
@@ -5,3 +5,13 @@
 # for every loop, ask for an input and add it to a variable named 'total'(total has an initial value of zero)
 # subtract total from the first number
 # print the result
+
+def difference_ten_numbers():
+    total = 0
+    first_number = int(input('Input the first number as the minuend (10): ')) # to divide the other numbers to
+    for _ in range(9):
+        number = int(input(f'Input the rest as a subtrahend : '))
+        total += number
+    return first_number - total
+    
+print(f'The first number minus the rest: {difference_ten_numbers()}')
