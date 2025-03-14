357e295105f46843bd9a4f938d042eaf6dbc4374
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 22:06:36 +0800

    Added the function

--- practice_set_3/display_only_unduplicating.py
+++ practice_set_3/display_only_unduplicating.py
@@ -10,3 +10,25 @@
 # for every loop, check if the item is in marked_as_duplicate
 # if it is, remove the item from the list
 # print the unduplicating list
+
+input_numbers = []
+unduplicating_list = []
+marked_as_duplicate = []
+
+for i in range(10):
+    user_input = input(f'Enter a number {i + 1}: ')
+    input_numbers.append(user_input)
+
+for numbers in input_numbers:
+    if numbers not in unduplicating_list:
+        unduplicating_list.append(numbers)
+    else:
+        marked_as_duplicate.append(numbers)
+    
+for number in unduplicating_list:
+    if number in marked_as_duplicate:
+        unduplicating_list.remove(number)
+        
+
+print(unduplicating_list)
+    
