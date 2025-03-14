8e7695bf3699e0b5cca0b1ca178e34c96aaab447
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 21:14:29 +0800

    Added function that reads the value between two numbers
    First the function asks for two numbers.
    Then, the function sets a floor number (minimum value) and a ceiling number (maximum value)
    It then runs a for loop between those values and prints all the numbers in between
    
    There's probably an easier way but this one works.

--- practice_set_2/numbers_between_two.py
+++ practice_set_2/numbers_between_two.py
@@ -8,3 +8,25 @@
 # else make it equal
 # run a for loop from the minimum(floor) value up to the maximum value(ceiling)
 # print the numbers in between
+
+def in_between():
+    first_input = input('Enter the first number: ')
+    second_input = input('Enter the second number: ')
+
+
+
+    if first_input < second_input: # if first input is smaller, make it as the minimum value
+        floor_num = first_input
+        ceiling_num = second_input
+    elif first_input > second_input: # if first input is larger, make it as the maximum value
+        ceiling_num = first_input
+        floor_num = second_input
+    else:
+        floor_num = first_input
+        ceiling_num = second_input    
+
+
+    for numbers_in_between in range(floor_num + 1, ceiling_num): # +1 on minimum number to make sure it is not included
+        print(numbers_in_between, end = ' ')
+
+
