7289e8c97922693ea3f8b2b2d4620320e0327722
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:40:54 +0800

    Added countdown
    Uses a counter that decrements every loop. This will appear as a countdown for the user to see how many numbers they have to input

--- practice_set_1/odd_number_counter.py
+++ practice_set_1/odd_number_counter.py
@@ -5,11 +5,13 @@
 
 def odd_numbers():
     odd_num_count = 0
+    counter = 10  # countdown, for auxillary purposes
     for _ in range(10):  # Asks for a number ------------------------
-        number = int(input(f'Input a number: '))
+        number = int(input(f'Input a number ({counter}): '))
         if number % 2 == 1:  # if odd, add 1 to the total amount of odd numbers and continue
             odd_num_count += 1
-    print(f' There are {odd_num_count} odd numbers')
+        counter -= 1  # countdown, for auxillary purposes
+    print(f'There are {odd_num_count} odd numbers')
 
 odd_numbers()
       
