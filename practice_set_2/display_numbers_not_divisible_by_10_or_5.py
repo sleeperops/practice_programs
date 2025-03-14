67738f0fe563256f898c1effbea729ba43ded19b
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 20:58:30 +0800

    Added pseudocode

---
+++ practice_set_2/display_numbers_not_divisible_by_10_or_5.py
@@ -0,0 +1,6 @@
+# prints all numbers from 0 - 100 not ending in 5 or 0
+
+# run a for loop that counts to 100
+# for every loop, check if the current number is divisible by 5 or 10
+# if it is, skip that number
+# else, print it
