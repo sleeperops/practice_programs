6bc8d9b77e5d2e413cbb77ad3e114051c1217ba2
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:24:24 +0800

    Added pseudocode

---
+++ practice_set_1/summation_of_ten_numbers.py
@@ -0,0 +1,4 @@
+# initialize a variable named 'total' and set it to zero
+# use a for loop to ask for a number ten times
+# for every iteration, add the input to the total
+# once ten iterations have completed, print the total
