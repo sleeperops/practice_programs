ed71e387a40f02074d51892f6f2b91fa47fdee1f
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:46:56 +0800

    Added function that prints only even numbers
    Function uses a for loop to run 101 times. If the current num is divisible by 2, it prints the number, if not, it continues through the loop

--- practice_set_1/even_numbers_displayer.py
+++ practice_set_1/even_numbers_displayer.py
@@ -3,3 +3,11 @@
 # if yes, print
 # else, continue the loop
 
+def only_even():
+    for num in range(101):
+        if num % 2 == 0:
+            print(num)
+        else:
+            continue
+   
+only_even()
