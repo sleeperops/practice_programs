f8020f2296dd21294a311ed9128b267f6f91f105
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 17:51:37 +0800

    Added error handling

--- practice_set_1/sum_of_two_numbers.py
+++ practice_set_1/sum_of_two_numbers.py
@@ -7,6 +7,13 @@
 
 input_a = input('Input the first number: ')
 input_b = input('Input the second number: ')
+try:
+    input_a = int(input_a)
+    input_b = int(input_b)
+except:
+    print('>>Not numbers')
+# ==============================================
+
 
 def sum_ab(a, b):
     return a + b
