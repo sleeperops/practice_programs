0b00a3693cba2153f636ef080d4b90e703c1ea30
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 20:01:29 +0800

    Added the function that gets the remainder

--- practice_set_2/remainder_of_two_numbers.py
+++ practice_set_2/remainder_of_two_numbers.py
@@ -1,5 +1,11 @@
+# '''prints the remainder'''
 # enter first input
 # enter second input
 # use modulo operator to get the remainder when the first number is divided by the second
 # print the result
 
+first_input = input('Enter the first number: ')
+second_input = input('Enter the second number: ')
+
+def remainder_r(input1,input2):
+    return input1 % input2
