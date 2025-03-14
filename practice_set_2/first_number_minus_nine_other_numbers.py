02ab54072b02d32088a4f9d3a6716b0983e02308
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 20:36:59 +0800

    Added pseudocode

---
+++ practice_set_2/first_number_minus_nine_other_numbers.py
@@ -0,0 +1,7 @@
+# prints the difference between the first number and the rest
+
+#enter the first number
+# use a for loop to get the value of the other 9 numbers
+# for every loop, ask for an input and add it to a variable named 'total'(total has an initial value of zero)
+# subtract total from the first number
+# print the result
