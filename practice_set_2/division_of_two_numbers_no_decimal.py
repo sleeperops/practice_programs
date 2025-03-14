513720f7a535b6e4c23a292e2d20a0c23880b52b
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 19:56:48 +0800

    Added pseudocode

---
+++ practice_set_2/division_of_two_numbers_no_decimal.py
@@ -0,0 +1,5 @@
+# '''prints the quotient of two numbers without the decimal'''
+# enter first input
+# enter second input
+# divide the first input by the second using floor division
+# display the output
