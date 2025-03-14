f781e4c3b668138eeb6d911da1eb35229ae0190a
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 18:05:16 +0800

    Added pseudocode

---
+++ practice_set_1/quotient_of_two_numbers.py
@@ -0,0 +1,10 @@
+# accept input for first input
+# accept input for second input
+# check if input is an integer, if not, display 'Not a number'
+
+# divide the first input by the second input
+# display the quotient
+
+
+
+
