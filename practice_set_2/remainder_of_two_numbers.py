114c188317c8a04c408a421cfe5341053ec86c07
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-14 20:01:48 +0800

    Added error handling

--- practice_set_2/remainder_of_two_numbers.py
+++ practice_set_2/remainder_of_two_numbers.py
@@ -9,3 +9,11 @@ second_input = input('Enter the second number: ')
 
 def remainder_r(input1,input2):
     return input1 % input2
+
+try:
+    first_input = int(first_input)
+    second_input = int(second_input)
+    print(f'remainder: {remainder_r(first_input, second_input)}')
+except:
+    print('>>Not a number')
+# ===============================================
