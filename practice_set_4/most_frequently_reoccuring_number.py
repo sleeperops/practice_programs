b94ba3664bcc31a782f937f4d92dc979d884ae0d
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-15 09:33:44 +0800

    Added pseudocode

---
+++ practice_set_4/most_frequently_reoccuring_number.py
@@ -0,0 +1,11 @@
+# initialize a list_called 'input_list'
+# initialize an 'occurence_counter_dict' dictionary to count how many times the number occured
+# initialize a variable named 'most_frequent' which describes how frequent a number appeared
+# run a for loop 10 times
+# for each loop, ask an input
+# append each input in input_list
+# run a for loop in the input_list
+# for every loop, check if the number is in the occurence_counter_dict
+# if not, append the number in occurence_counter_dict as a key with a value of 1
+# > the key will refer to the number, and the value will refer to the occurence 
+# if it already exist in occurence_counter_dict, add 1 to the value, implying that the number only appeared once
