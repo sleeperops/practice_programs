2430e63b148d7cd5164bbd88731ae3d50ccf14ad
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-15 12:17:23 +0800

    Added the ability to search for the number with the most duplicate
    Previous version threw an error identifying the most frequently occurring input. This might be because the variable that stores the most frequently occurring input is not a list. This means that if two or more inputs have the same highest_value, the variable gets overwritten resulting in unexpected behaviours.
    
    Another reason as to why it failed might be because the program run like this:
        search dictionary for keys with highest value -> if the current key has the highest value -> set it as the most reoccurring input (number with most duplicates)
    > This presents a problem since I am setting the highest value while using it as a standard at the same time
     So the highest_value keeps changing as the program loops
    
    which I renewed into this:
        search the dictionary for the highest value first -> then run another for loop and search for the key with the same corresponding value -> store all those keys in a list.
    > This ensures that the highest_value remains constant

--- practice_set_4/most_frequently_reoccuring_number.py
+++ practice_set_4/most_frequently_reoccuring_number.py
@@ -1,18 +1,25 @@
+# displays the input with the most numbers of duplicates(most freqeuntly occuring inputs)
+
 # initialize a list_called 'input_list'
 # initialize an 'occurrence_counter_dict' dictionary to count how many times the number occured
-# initialize a variable named 'most_frequent' which describes how frequent a number appeared
 # run a for loop 10 times
 # for each loop, ask an input
 # append each input in input_list
 # run a for loop in the input_list
 # for every loop, check if the number is in the occurrence_counter_dict
 # if not, append the number in occurrence_counter_dict as a key with a value of 1
-# > the key will refer to the number, and the value will refer to the occurence 
+# > the key will refer to the number, and the value will refer to the occurence of that number
 # if it already exist in occurrence_counter_dict, add 1 to the value, implying that the number only appeared once
+
+# initialize a list named 'most_frequent' which describes the most frequently occuring numbers
+# initialize a variable named 'highest_value' which describes the highest value in the dictionary
+# In the occurrence_counter_dict, search for the keys with the highest value and store it as highest_value
+# Search occurence_counter_dict for all the keys with the highest values, store them in most_frequent 
+# print most_frequent to display all of the most frequently occuring numbers
 
 input_list = []
 occurrence_counter_dict = {}
-most_frequent = 0
+
 
 
 # gets input and stores it in a list
@@ -29,17 +36,20 @@ for number in input_list:
     else:
         occurrence_counter_dict[number] = 1
         
-# Loops through the disctionary. Checks if the current key if it has the highest value
+# Loops through the dictionary. Checks the current key if it has the highest value, store the highes_value in a variable. Finally, search every key that has that value
+most_frequent = []
+highest_value = 0
 for key in occurrence_counter_dict:
-    if int(occurrence_counter_dict[key]) > most_frequent:
-        most_frequent = key
+    if occurrence_counter_dict[key] > highest_value: # removed unnecessary casting
+        highest_value = occurrence_counter_dict[key]
 
+# check all the keys with the same value as the highest_value and append them to the most_frequent list
+for key in occurrence_counter_dict:
+    if occurrence_counter_dict[key] == highest_value:
+        most_frequent.append(key)
+
+print(f'highest_value = {highest_value}')
 print(f'Most frequent = {most_frequent}')
-
-print(occurrence_counter_dict)
-print(input_list)
-   
-    
 
 print(occurrence_counter_dict) # test
 print(input_list) # test
