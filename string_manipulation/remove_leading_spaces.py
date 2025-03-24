6dea3bcb17dfdeed4d60b38727484ca888d0c728
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-24 22:46:01 +0800

    Added ability to strip leading whitespaces

--- string_manipulation/remove_leading_spaces.py
+++ string_manipulation/remove_leading_spaces.py
@@ -1,3 +1,7 @@
 # get user input
 # use lstrip (function? method?) to remove leading whitespace
 # print input
+
+user_input = input('Enter a sentence with leading spaces: ')
+user_input.lstrip()
+print(user_input)
