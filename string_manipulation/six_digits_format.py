6c06a01ba39e635a25095d006c3b9659a26655e3
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-24 23:24:16 +0800

    Used f string to format the numbers into six digits

--- string_manipulation/six_digits_format.py
+++ string_manipulation/six_digits_format.py
@@ -1,1 +1,7 @@
-6 digits
+# Ask for input 
+# Use f-string to format number to a 6 digit format
+# print the result
+
+user_input = int(input('Enter a number from 0-1000: '))
+six_digit = f'{user_input:06d}'
+print(six_digit)
