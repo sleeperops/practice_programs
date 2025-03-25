a385c77a01016c834bf49efde8825acea02ca1d7
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-25 13:45:59 +0800

    Created pascal_case.py and added the pseudocode

---
+++ string_manipulation/pascal_case.py
@@ -0,0 +1,4 @@
+# Prompt user to input their fullname in incorrect casing.
+# Convert the casing into title case.
+# Store each word into a list using .split().
+# Rejoin the list into a single text using the .join() method.
