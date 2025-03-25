a2555c37b0b6a26fff0cc0366350a1e98fad4991
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-25 13:09:38 +0800

    Used enumerate to count

--- string_manipulation/word_counter.py
+++ string_manipulation/word_counter.py
@@ -1,3 +1,9 @@
-# Prompt the user to enter a sentence.
+# Prompt the user to enter a complete statement.
+statement = input("Enter a complete statement: ")
+
 # Use .split() method to split the senteces into a list of words
+word_list = statement.split(" ")
+
 # Use enumerate() to count the items in the list
+# Print the result
+print(enumerate(word_list))
