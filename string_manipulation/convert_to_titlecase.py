389d6002ce8b4e13b4d47fb8a1d455eae359fff9
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-25 12:32:43 +0800

    Applied .title() to convert the text into titlecase

--- string_manipulation/convert_to_titlecase.py
+++ string_manipulation/convert_to_titlecase.py
@@ -1,3 +1,5 @@
 # Ask for full name in improper casing
+fullname = input("Enter fullname in improper casing: ")
 # Use .title() to convert the full name into title case
 # Print the result
+print(fullname.title())
