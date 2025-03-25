ea10e8d735c277a62c5102e739a1cee545121ba2
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-25 10:44:03 +0800

    Applied lower.() to convert the input to lowercase

--- string_manipulation/convert_to_lowercase.py
+++ string_manipulation/convert_to_lowercase.py
@@ -1,3 +1,5 @@
 # Prompt the user to enter their full name
 # Apply .lower() to convert full name into lowercase
 # Print the result 
+full_name = input("Enter your full name: ")
+print(full_name.lower())
