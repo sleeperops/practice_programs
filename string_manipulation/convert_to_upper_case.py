24af7598fe57f0c03d4772ba3ac8bf2a4e4d2122
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-25 10:09:11 +0800

    Added functionality
    > Applied .upper() to capitalize fullname
    Renamed the file

--- string_manipulation/convert_to_upper_case.py
+++ string_manipulation/convert_to_upper_case.py
@@ -1,1 +1,6 @@
- 
+# Ask for user to enter their name
+# Apply .upper() to convert all characters to uppercase
+# print the result
+
+user_input = input('Enter your full name: ')
+print(user_input.upper())
