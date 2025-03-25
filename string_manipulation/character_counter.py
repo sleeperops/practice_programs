ffab472f5763e496bca7771bb4614f581081d288
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-25 13:28:08 +0800

    Used a for loop to count the characters in the input

--- string_manipulation/character_counter.py
+++ string_manipulation/character_counter.py
@@ -1,5 +1,11 @@
 # Prompt the user to enter their fullname
-# Loop through the input and count each characters
-# Print the result
+fullname = input('Enter your fullname: ')
 
+# Loop through the input. Use a counter to record each occurrence of a character
+counter = 0
+for character in fullname:
+    counter += 1
+
+# Print the result
+print(counter)
 
