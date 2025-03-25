fb96c49cd7eb9da67b8c6f5539b374db6dce75c3
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-25 09:59:31 +0800

    Created convert_to_upper_case.py
    > Added pseudocode for the program
    Additionally, remove_leading_spaces.py
    have been reorganized for readability

--- string_manipulation/remove_leading_spaces.py
+++ string_manipulation/remove_leading_spaces.py
@@ -1,6 +1,6 @@
 # get user input
+user_input = input('Enter a sentence with leading spaces: ')
+
 # use lstrip (function? method?) to remove leading whitespace
 # print input
-
-user_input = input('Enter a sentence with leading spaces: ')
 print(user_input.lstrip())
