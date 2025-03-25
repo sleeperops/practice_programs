a58586012027c94722d0bb8bdd2aa55a41b8530b
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-25 14:04:19 +0800

    Applied .lower(), .split(), and .join()
    Correction for the last commit: .lower() is used instead of .title() since the
    goal is to convert the fullname into lowercase first. Another difference from pascal_case.py
    is that it uses an underscore to join the words instead of none.

--- string_manipulation/snake_case.py
+++ string_manipulation/snake_case.py
@@ -1,5 +1,14 @@
 # Prompt user to input their fullname in incorrect casing.
-# Convert the casing into title case using .title().
+fullname = input("Enter your fullname in incorrect casing: ")
+
+# Convert the casing into title case using .lower().
+fullname = fullname.lower()
+
 # Store each word into a list using .split().
+word_list = fullname.split()
+
 # Rejoin the list into a single text using the .join() method.
-# Print the result. 
+snake = '_'.join(word_list)
+
+# Print the result.
+print(snake)
