19a922a03594f1ca54414f6a28452baca0d6c199
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-25 14:02:09 +0800

    Created snake_case.py and added pseudocode

---
+++ string_manipulation/snake_case.py
@@ -0,0 +1,5 @@
+# Prompt user to input their fullname in incorrect casing.
+# Convert the casing into title case using .title().
+# Store each word into a list using .split().
+# Rejoin the list into a single text using the .join() method.
+# Print the result. 
