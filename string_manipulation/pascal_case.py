e62be021bac14bbdb0a304fac2d1e528e8b33b22
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-25 13:52:58 +0800

    Applied .title(), .split(), and .join()
    Used .title() to first convert it to title case. The method .split() is then applied to
    store the words in a list so it can be recombined without the space. It is then recombined
    using .join().

--- string_manipulation/pascal_case.py
+++ string_manipulation/pascal_case.py
@@ -1,4 +1,14 @@
 # Prompt user to input their fullname in incorrect casing.
-# Convert the casing into title case.
+fullname = input("Enter your fullname in incorrect casing: ")
+
+# Convert the casing into title case using .title().
+fullname = fullname.title()
+
 # Store each word into a list using .split().
+word_list = fullname.split()
+
 # Rejoin the list into a single text using the .join() method.
+pascal = ''.join(word_list)
+
+# Print the result.
+print(pascal)
