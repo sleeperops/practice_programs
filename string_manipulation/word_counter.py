2c66dc84c18680cb1a7939597fdff8fd3a2cc1e0
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-25 13:15:41 +0800

    Fixed the output being the function itself
    Instead of using enumerate(), a for loop with a counter is instead applied
    to count the words in the statement. The enumerate line did not make sense.

--- string_manipulation/word_counter.py
+++ string_manipulation/word_counter.py
@@ -1,9 +1,13 @@
 # Prompt the user to enter a complete statement.
 statement = input("Enter a complete statement: ")
 
-# Use .split() method to split the senteces into a list of words
+# Store each word from the statement to the list using .split() method
 word_list = statement.split(" ")
 
-# Use enumerate() to count the items in the list
-# Print the result
-print(enumerate(word_list))
+# Use a for loop and a counter to count the number of words in the list.
+counter = 0
+for word in word_list:
+    counter += 1
+
+# Print the result 
+print(f"There are {counter} words in that statement.")
