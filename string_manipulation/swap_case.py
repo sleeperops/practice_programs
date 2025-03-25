dac26a2737ed94c0577ca4cd349e8c559131aba4
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-25 12:53:16 +0800

    Applied .swapcase() to reverse the casings
    > Additionally, the comments are reorganized for readability

--- string_manipulation/swap_case.py
+++ string_manipulation/swap_case.py
@@ -1,5 +1,5 @@
- # Prompt the user to enter their name in incorrect casing
- # Apply the method .swapcase() to reverse the casing
- # Print the result
+# Prompt the user to enter their name in incorrect casing
+fullname = input("Enter your fullname in incorrect casing: ")
 
- 
+# Apply the method .swapcase() to reverse the casing of the fullname. Print the result.
+print(fullname.swapcase())
