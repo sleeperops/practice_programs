5018cc219d5e4a51f3e5c721c1bb639fa6f495d2
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-28 18:53:14 +0800

    Added the functionality
    It uses the 'state' variable to determine where the actual text starts.

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicating_lstrip.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicating_lstrip.py
@@ -6,3 +6,16 @@
 # Once it sees a non-whitespace character, set the state to 1 and write the character into 'output_string'
 # After the state is set to 1, the program will never skip anymore, which means the rest of the character will be written.
 
+input_string = input("Enter a text with spaces at the beginning: ")
+output_string = ""
+state = 0
+for characters in input_string:
+    if characters == " " and state == 0:
+        continue
+    elif characters != " ": # use or?
+        state = 1
+        output_string += characters
+    elif state == 1:
+        output_string += characters
+
+print(output_string)
