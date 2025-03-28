11e5c1bf86107844390b85ac0436d94c9dfc1582
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-28 22:18:45 +0800

    Added functionality and cleaned the code as well

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicate_isupper.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_isupper.py
@@ -1,1 +1,8 @@
- 
+# Prompts the user to enter a sentence.
+input_text = input("Enter a sentence to check if it's in uppercase or not: ")
+
+# Checks if the input_text is the same as its uppercase version. 
+if input_text == input_text.upper():
+    print("True")
+else:
+    print("False")
