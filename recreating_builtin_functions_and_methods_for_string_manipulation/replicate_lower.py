2cac1dbc261cd1519f15af684cc105eee65f74bb
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-28 22:02:27 +0800

    Cleaned code and added comments

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicate_lower.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_lower.py
@@ -1,10 +1,11 @@
 # Prompt the user to enter a text in random casing
-# Apply .upper() on the text to capitalize all characters
-# Apply .swapcase() to reverse the casing
-# Print the result
-
 text_input = input("Input a text in random casing: ")
 
+# Apply .upper() on the text to capitalize all characters
 text_input = text_input.upper()
+
+# Apply .swapcase() to reverse the casing
 text_input = text_input.swapcase()
+
+# Print the result
 print(text_input)
