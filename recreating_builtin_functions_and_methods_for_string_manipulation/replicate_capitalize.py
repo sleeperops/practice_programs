1b8b5752b4f9c4105481ccee11c538d4b85a673c
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-28 22:55:09 +0800

    Applied .lower() and .replace()

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicate_capitalize.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_capitalize.py
@@ -1,4 +1,9 @@
 # Prompt user to enter a text.
 # Apply .lower() to the string.
-# Apply .upper() to the first index of the string.
+# Use .replace() to replace the first index of the string with an uppercase version.
 # Print the result.
+
+text_input = input("Enter a text in any casing: ")
+text_input = text_input.lower()
+text_input = text_input.replace(text_input[0], text_input[0].upper())
+print(text_input)
