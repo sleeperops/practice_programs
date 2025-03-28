2be19c35410dbee594067c39a604da2d8202d0d1
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-28 19:13:07 +0800

    Refactored code and did some cleaning

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicating_lstrip.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicating_lstrip.py
@@ -1,21 +1,18 @@
 # The idea is that as long as the program is looking at a whitespace, it will skip.
-# Once it sees a character that isnt a whitespace, it will start writing those characters.
+# Once it sees a character that isnt a whitespace, it will start writing the rest, regardless if it encounters a whitespace again.
 
-# Prompt the user to input a text
-# Loop through the text, if both the character is a whitespace and the state is equal to 0, skip
-# Once it sees a non-whitespace character, set the state to 1 and write the character into 'output_string'
-# After the state is set to 1, the program will never skip anymore, which means the rest of the character will be written.
-
+# Prompts the user to input a text
 input_string = input("Enter a text with spaces at the beginning: ")
 output_string = ""
 state = 0
+
+# Loops through the characters in the text.
 for characters in input_string:
-    if characters == " " and state == 0:
+    if characters == " " and state == 0: # If both the character is a whitespace and state = 0, skip.
         continue
-    elif characters != " ": # use or?
+    elif characters != " " or state == 1: # If either the character a non-whitespace or state = 1, write.
         state = 1
         output_string += characters
-    elif state == 1:
-        output_string += characters
 
+# Print result.
 print(output_string)
