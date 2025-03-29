ef2fef7f3384e3f6e9ee003031c52914dcaf13ff
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-29 11:39:05 +0800

    Added functionality. Applies .lower() and .upper()

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicate_swapcase.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_swapcase.py
@@ -2,3 +2,16 @@
 # Loop through the text. If the character is in uppercase, create a lowercase version and write it into the output_text
 # Else if the character is in lowercase, create an uppercase version and write it into the output_text
 # Print the output
+
+text = "this IS A tEsT StRiNG"
+output_text = ""
+
+for char in text:
+    if char == char.upper(): # character is in uppercase
+        char = char.lower()
+        output_text += char
+    elif char == char.lower(): # character is in lowercase
+        char = char.upper()
+        output_text += char
+print(text)
+print(output_text)
