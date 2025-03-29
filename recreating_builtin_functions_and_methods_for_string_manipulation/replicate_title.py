4ec32f17e5744c70aa30d1d068d2db7107bcc535
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-29 15:34:52 +0800

    Reformed the functionality.
    The program splits the string into words, modifies it and then manually
    writes each letter into the output_text. This works but is extremely
    cumbersome and fails when multiple spaces are presented.

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicate_title.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_title.py
@@ -1,20 +1,30 @@
 # # Prompt the user to enter a text: 
 # Split the text into words and store that into a list.
-# Loop through each list and capitalize the first letter of each word.
-# Rejoin the list into as a single text and print the output. 
+# Loop through the words in list.
+# First capitalize the first letter, write it into the output_text.
+# Second, write the rest of the letters and write it into the output_text.
+# Lastly, add the space at the end.
+# Print the resulting text.
 
-text = "What a beautiful face"
+# User input
+text = input("Input a text to be converted into titlecase: ")
 text = text.lower()
-output_text = []
+output_text = ""
 
 # split text into a list
 word_list = text.split() 
 print(word_list)
 
-# Capitalize each first letter of the word
+# Loop through every word in the list
 for word in word_list:
-    word[0].upper()
-    output_text.append(word[0].upper())
+    output_text += word[0].upper() # Write the first letter in uppercase
+    output_text += word[1:] # Writes the rest of the letters
+    output_text += " " # Writes the space 
 
-# rejoin the list into a text
-print(" ".join(output_text))
+# Simply removes the extra space at the end that is produced in the loop.
+output_text.rstrip()
+
+# Prints the result.
+
+print(text)
+print(output_text)
