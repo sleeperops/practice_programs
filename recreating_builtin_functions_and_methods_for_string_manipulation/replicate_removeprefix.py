f8b964e3c3e8dae7314d0da83688e28afd89a377
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-29 18:10:46 +0800

    Create replicate_removeprefix.py and added pseudocode

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicate_removeprefix.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_removeprefix.py
@@ -1,1 +1,6 @@
- 
+# Prompt the user to enter a text as input
+# Prompt the user to enter the prefix to be removed
+# Loop through the prefix. For every character, search the user_input for the first occurrence of that character
+# Remove it from the user_input.
+# Print the result
+
