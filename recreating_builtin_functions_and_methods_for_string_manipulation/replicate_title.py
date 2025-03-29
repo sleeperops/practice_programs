17f253d17768a6ed90028bab51f93e551b17a036
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-29 17:35:49 +0800

    Revised the entire thing
    Instead of using a split, modify, join approach, it instead used a
    similar method as replicate_ lstrip.py. That is, by using a variable
    called state that detects wether the previous string is a whitespace or
    a non-whitespace.
    
    If the current character is a whitespace, it will keep the state to zero
    letting it know that it is a whitespace. If the previous character is a
    whitespace, it means the current character that is being iterated is the
    first letter in a word. This way, it knows what part of the string have
    to be capitalized.

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicate_title.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_title.py
@@ -1,30 +1,25 @@
-# # Prompt the user to enter a text: 
-# Split the text into words and store that into a list.
-# Loop through the words in list.
-# First capitalize the first letter, write it into the output_text.
-# Second, write the rest of the letters and write it into the output_text.
-# Lastly, add the space at the end.
-# Print the resulting text.
+# Prompt the user to enter an input as a text.
+# Convert the text to lowercase.
+# Initialize variables 'state' and 'output_text'.
+# Loop through the text and do the following:
+#    If the the program detects a whitespace, set the state to 0 and write the whitespace to the output_text. Keep state as 0.
+#    Elif the program detects a non-whitespace but the state is 0 (meaning it is the first letter in the word.), capitalize it and
+#         write it to the output_text. Increment state by 1.
+#    Elif it is a non whitespace, and the state is 1 or more, write it to output_text. Increment state by 1.
+# Print the result
 
-# User input
-text = input("Input a text to be converted into titlecase: ")
-text = text.lower()
+text = input("Enter a text to be converted into titlecase: ").lower()
+state = 0
 output_text = ""
 
-# split text into a list
-word_list = text.split() 
-print(word_list)
-
-# Loop through every word in the list
-for word in word_list:
-    output_text += word[0].upper() # Write the first letter in uppercase
-    output_text += word[1:] # Writes the rest of the letters
-    output_text += " " # Writes the space 
-
-# Simply removes the extra space at the end that is produced in the loop.
-output_text.rstrip()
-
-# Prints the result.
-
-print(text)
+for char in text:
+    if char == " ": # if whitespace
+        state = 0
+        output_text += char
+    elif char != " " and state == 0: # not whitespace and is first letter in the word
+        state += 1
+        output_text += char.upper() 
+    elif char != " " and state >= 1:
+        state += 1
+        output_text += char
 print(output_text)
