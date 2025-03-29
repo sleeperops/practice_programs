7082cddfe66f6f0cb785a989afa39a383a825568
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-29 11:37:11 +0800

    created replicate_swapcase.py

---
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_swapcase.py
@@ -0,0 +1,4 @@
+# Prompt the user to input a text 
+# Loop through the text. If the character is in uppercase, create a lowercase version and write it into the output_text
+# Else if the character is in lowercase, create an uppercase version and write it into the output_text
+# Print the output
