eb0ce0ff05c524c3b2922479ad484060cc1d7944
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-29 15:17:22 +0800

    Created replicate_title.py and added pseudocode

---
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_title.py
@@ -0,0 +1,4 @@
+# # Prompt the user to enter a text: 
+# Split the text into words and store that into a list.
+# Loop through each list and capitalize the first letter of each word.
+# Rejoin the list into as a single text and print the output. 
