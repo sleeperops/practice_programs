e9cde26a68fe58e67594ef7a02ea08d23f7e2b9c
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-29 15:19:54 +0800

    Added functionality. Uses split and rejoin.

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicate_title.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_title.py
@@ -2,3 +2,19 @@
 # Split the text into words and store that into a list.
 # Loop through each list and capitalize the first letter of each word.
 # Rejoin the list into as a single text and print the output. 
+
+text = "What a beautiful face"
+text = text.lower()
+output_text = []
+
+# split text into a list
+word_list = text.split() 
+print(word_list)
+
+# Capitalize each first letter of the word
+for word in word_list:
+    word[0].upper()
+    output_text.append(word[0].upper())
+
+# rejoin the list into a text
+print(" ".join(output_text))
