8201da1e3ab40da685cf66aa4a981470445cd0f5
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-29 17:59:54 +0800

    Cleaned and reorganized the code

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicate_title.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_title.py
@@ -1,25 +1,21 @@
-# Prompt the user to enter an input as a text.
-# Convert the text to lowercase.
-# Initialize variables 'state' and 'output_text'.
-# Loop through the text and do the following:
-#    If the the program detects a whitespace, set the state to 0 and write the whitespace to the output_text. Keep state as 0.
-#    Elif the program detects a non-whitespace but the state is 0 (meaning it is the first letter in the word.), capitalize it and
-#         write it to the output_text. Increment state by 1.
-#    Elif it is a non whitespace, and the state is 1 or more, write it to output_text. Increment state by 1.
-# Print the result
-
+# Prompt the user to enter an input as a text. Convert it to lowercase
 text = input("Enter a text to be converted into titlecase: ").lower()
+
+# Initialize variables 'state' and 'output_text'.
 state = 0
 output_text = ""
 
+
 for char in text:
-    if char == " ": # if whitespace
+    if char == " ": # If the current character is a whitespace
         state = 0
-        output_text += char
-    elif char != " " and state == 0: # not whitespace and is first letter in the word
+        output_text += char # Write the character
+    elif char != " " and state == 0: # If the current character is not a whitespace but the previous character is a whitespace
         state += 1
-        output_text += char.upper() 
-    elif char != " " and state >= 1:
+        output_text += char.upper() # Capitalize and write the character
+    elif char != " " and state >= 1: # If the current and previous characters are not whitespaces
         state += 1
-        output_text += char
+        output_text += char # Just write the character
+
+# Print the result
 print(output_text)
