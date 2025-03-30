37d5b3d95debb48cc284c4b16f9cb7967ebf5799
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-30 12:45:13 +0800

    Added new functionality
    It checks if the input starts with the prefix. If it does, it prints
    every text after the prefix.

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicate_removeprefix.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_removeprefix.py
@@ -1,5 +1,10 @@
-# Prompt the user to enter a text as input
-# Prompt the user to enter the prefix to be removed
-# Apply startswith() method to check if the input starts with the prefix
-# If it does, print the result.
+# Prompt the user to enter a text as input. 
+user_input = input("Enter a text: ")
 
+# Prompt the user to enter the prefix to be removed
+prefix = input("Enter the prefix to be removed: ")
+
+# Apply startswith() method to check if the input starts with the prefix
+if user_input.startswith(prefix):
+    output = user_input[len(prefix):]
+    print(output) #If it does, print the result.
