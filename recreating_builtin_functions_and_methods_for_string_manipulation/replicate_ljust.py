0847640d8c0a98a2258c22539c4a6617e86d3c51
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-31 20:30:32 +0800

    Created replicate_ljust.py and added initial pseudocode

---
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_ljust.py
@@ -0,0 +1,5 @@
+# Prompt the user to enter sample_text
+# Prompt the user to enter the total string length
+# Prompt the user to enter the char that will be used to justify the sample_text
+# Get the difference between the desired string length and length of the characters in the sample_text.
+# Print the text and add the difference at the end.
