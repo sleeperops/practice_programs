ab3a4b191d87075f5d7a99dfc905b80e732c291a
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-31 22:10:07 +0800

    Reorganized code into a function. Cleaned pseudocode.

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicate_ljust.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_ljust.py
@@ -1,5 +1,16 @@
 # Prompt the user to enter sample_text
+sample_text = input("Enter a sample text: ")
+
 # Prompt the user to enter the total string length
-# Prompt the user to enter the char that will be used to justify the sample_text
-# Get the difference between the desired string length and length of the characters in the sample_text.
-# Print the text and add the difference at the end.
+target_length = int(input("Enter the length of the text: "))
+
+# Prompt the user to enter the character that will be used to fill the extra spaces 
+padding = input("Enter the padding/filler: ")
+
+# Compare the target length 
+def mock_ljust(sample_text, length, padding):
+    pad_ammount = length - len(sample_text) # Determines how much padding is needed to be printed
+    return f"{sample_text}{str(padding * pad_ammount)}"
+
+# Print the result
+print(mock_ljust(sample_text, target_length, padding))
