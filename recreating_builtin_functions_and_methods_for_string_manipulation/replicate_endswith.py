97ecad7e541b8b377c5884a16bba66e5354ac5e6
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-03-31 11:56:38 +0800

    Revised with new functionality
    The program should work in this manner: Starting from right to
    left, compare the characters of the given input text and the suffix.
    If one character from the suffix fails to match its counter-part, break
    the loop and return FALSE. However, if the loop stops without any
    discrepancy, return TRUE.

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicate_endswith.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_endswith.py
@@ -1,14 +1,23 @@
 # Prompt the user to input a text
 # Prompt the user to input the suffix 
-# Run a backwards for loop through the suffix 
+# From right to left, search through the suffix and compare it with the input text
+# As long as the characters match, keep on going.
+# If one character does not match, return FALSE.
+# If the loop finished without discepancies, return TRUE.
+# Call the function and print the result.
 
-text = input("Enter a text: ")
+input_text= input("Enter a text: ")
 suffix = input("Enter the suffix: ")
 
-# Starting from right to left, loop through the suffix and compare it to the original input
-for suffix_char in suffix[::-1]:
-    for letters in text[::-1]: # Compares the suffix to the original input
-        if suffix_char == letters: # If the a character in the suffix is equal to that of the input
-            print("True")
-    else: # if the condition is not met
-        print("False")
+def better_endswith(input_text, suffix):
+    for input_char in suffix[::-1]:
+        for suffix_char in input_text[::-1]:
+            if suffix_char != input_char:
+                print("FALSE")
+                break
+        else:
+            print("TRUE")
+
+
+
+better_endswith(input_text, suffix)
