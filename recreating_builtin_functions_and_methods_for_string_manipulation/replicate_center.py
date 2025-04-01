d4f9e2e4b6693d4f6eb29d003d5b6fb4d9a42ee9
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-01 12:47:53 +0800

    Created replicate_center.py and added pseudocode

---
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_center.py
@@ -0,0 +1,7 @@
+# Prompt the user to enter a sample text, the length of the text and the padding character which will be used to center the text
+# Initialize text_input as the sample text, length_input as the length of the sentence and pad_char_input as the padding character.
+# To determine the ammount of padding that will be used to center the text, subtract length_input from len(text_input)
+# Divide it by two. In the case of odd numbers wherein the difference is not equal, store the bigger half into 'prefix'.
+# Store the smaller half into 'suffix'.
+# # Print the prefix at the start, the text_input in the middle and the suffix at the end.
+
