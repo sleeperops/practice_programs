3b843d6e70da3f84db10e92bf6c15bf6df4eab14
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-01 13:13:00 +0800

    Added functionality
    Gets the input text, the target length of the text and the padding to
    be used to center the text. Determine how much padding needs to be used.
    Print the text with the padding at the start as the prefix, and the one
    at the end as the suffix.

--- recreating_builtin_functions_and_methods_for_string_manipulation/replicate_center.py
+++ recreating_builtin_functions_and_methods_for_string_manipulation/replicate_center.py
@@ -5,3 +5,19 @@
 # Store the smaller half into 'suffix'.
 # # Print the prefix at the start, the text_input in the middle and the suffix at the end.
 
+text_input = input("Enter a sample text: ")
+length_input = int(input("Enter the target length of the text: "))
+pad_char_input = input("Enter the padding character that will be used to fill the text: ")
+
+def mock_center(text, length, pad):
+    pad_ammount = length - len(text)
+    if pad_ammount % 2 == 1: # if odd
+        prefix = -(-pad_ammount // 2) # This divides the pad_ammount by 2 and rounds it up
+        suffix = (pad_ammount//2) # This divides the pad_ammount by 2 and rounds it down
+    else: 
+        prefix = pad * (int(pad_ammount /2))
+        suffix = pad * (int(pad_ammount /2))
+    
+    return f"{prefix}{text}{suffix}"
+
+print(mock_center(text_input, length_input, pad_char_input))
