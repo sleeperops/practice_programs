0a254e9b3798985abe456382f72c2c8ad5392188
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-02 19:41:42 +0800

    Removed state functionality

--- replicating_string_methods_2/replicate_rstrip.py
+++ replicating_string_methods_2/replicate_rstrip.py
@@ -1,21 +1,21 @@
 # Similar to lstrip, except the loop starts from right to left and not the other way around 
 # The idea is that as long as the program is looking at a whitespace, it will skip.
-# Once it sees a character that isnt a whitespace, it will start writing the rest, regardless if it encounters a whitespace again.
+# Once it sees a character that isnt a whitespace, it will start writing the rest.
 
 # Prompts the user to input a text
 input_string = input("Enter a text with spaces at the end: ")
 
 
-def mock_lstrip(input_string):
+def mock_rstrip(input_string):
+    input_string = str(input_string) # To allow the function to use string methods like index
     output_string = ""
-    state = 0
     for characters in input_string[::-1]: # Loops through the characters in the input_text.
-        if characters == " " and state == 0: # If both the character is a whitespace and state = 0, skip.
+        if characters == " ": # If both the character is a whitespace and state = 0, skip.
             continue
-        elif characters != " " or state == 1: # If either the character a non-whitespace or state = 1, write.
+        elif characters != " ": # If either the character a non-whitespace or state = 1, write.
             state = 1
-            output_string += characters
-    return output_string
+            break
+    return input_string[0:input_string.index(characters)+1] # returns from the start without the whitespace at the end
 
 # Print result.
-print(f"{mock_lstrip(input_string)}This should be close to the text")
+print(f"{mock_rstrip(input_string)}This should be close to the text")
