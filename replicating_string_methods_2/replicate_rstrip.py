d57f1d0f1235b73a29ff55ebb94aca15b2a89718
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-02 11:40:23 +0800

    Added functionality similar to replicate_lstrip

--- replicating_string_methods_2/replicate_rstrip.py
+++ replicating_string_methods_2/replicate_rstrip.py
@@ -1,8 +1,19 @@
+# Similar to lstrip, except the loop starts from right to left and not the other way around 
 # The idea is that as long as the program is looking at a whitespace, it will skip.
 # Once it sees a character that isnt a whitespace, it will start writing the rest, regardless if it encounters a whitespace again.
 
 # Prompts the user to input a text
-# Loops through the characters in the text, backwards.
-# If both the character is a whitespace and state = 0, skip.
-# If either the character a non-whitespace or state = 1 , write.
+input_string = input("Enter a text with spaces at the beginning: ")
+output_string = ""
+state = 0
+
+# Loops through the characters in the text.
+for characters in input_string:
+    if characters == " " and state == 0: # If both the character is a whitespace and state = 0, skip.
+        continue
+    elif characters != " " or state == 1: # If either the character a non-whitespace or state = 1, write.
+        state = 1
+        output_string += characters
+
 # Print result.
+print(output_string)
