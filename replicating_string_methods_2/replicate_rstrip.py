c85fd9d2c279b210d2bcce967cb19ee190ebc8e8
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-02 11:46:42 +0800

    Tried to adapt to lstrip code but needs more adjustment

--- replicating_string_methods_2/replicate_rstrip.py
+++ replicating_string_methods_2/replicate_rstrip.py
@@ -3,17 +3,19 @@
 # Once it sees a character that isnt a whitespace, it will start writing the rest, regardless if it encounters a whitespace again.
 
 # Prompts the user to input a text
-input_string = input("Enter a text with spaces at the beginning: ")
-output_string = ""
-state = 0
+input_string = input("Enter a text with spaces at the end: ")
 
-# Loops through the characters in the text.
-for characters in input_string:
-    if characters == " " and state == 0: # If both the character is a whitespace and state = 0, skip.
-        continue
-    elif characters != " " or state == 1: # If either the character a non-whitespace or state = 1, write.
-        state = 1
-        output_string += characters
+
+def mock_lstrip(input_string):
+    output_string = ""
+    state = 0
+    for characters in input_string[::-1]: # Loops through the characters in the input_text.
+        if characters == " " and state == 0: # If both the character is a whitespace and state = 0, skip.
+            continue
+        elif characters != " " or state == 1: # If either the character a non-whitespace or state = 1, write.
+            state = 1
+            output_string += characters
+    return output_string
 
 # Print result.
-print(output_string)
+print(f"{mock_lstrip(input_string)}This should be close to the text")
