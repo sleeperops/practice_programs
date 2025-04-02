d42c47b549d78038c0ad373937ff36654baf504b
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-02 23:13:30 +0800

    Created functionality

--- replicating_string_methods_2/replicate_islower.py
+++ replicating_string_methods_2/replicate_islower.py
@@ -1,5 +1,10 @@
 # Prompt the user to enter a text
-# If the text is equal to its lowercase counterpart, return FALSE
-# Otherwise, return TRUE
+input_text = input("Enter a text: ")
 
-
+def mock_islower(text):
+    if text == text.lower(): # If the text is equal to its lowercase counterpart, return TRUE
+        return True
+    else:
+        return False # Otherwise, return FALSE
+    
+print(mock_islower(input_text))
