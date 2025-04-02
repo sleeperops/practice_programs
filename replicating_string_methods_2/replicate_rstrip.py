7d37218330b94dad46045b21c9484b47d4c366dc
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-02 21:48:13 +0800

    Fixed some comments

--- replicating_string_methods_2/replicate_rstrip.py
+++ replicating_string_methods_2/replicate_rstrip.py
@@ -1,14 +1,15 @@
 # Similar to lstrip, except the loop starts from right to left and not the other way around 
 # The idea is that as long as the program is looking at a whitespace, it will skip.
-# Once it sees a character that isnt a whitespace, it will start writing the rest.
+# Once it sees a character that isnt a whitespace, it returns the start of the input until the last iteration.
 
 # Prompts the user to input a text
 input_string = input("Enter a text with spaces at the end: ")
 
 def mock_rstrip(input_string):
     input_string = str(input_string) # To allow the function to use string methods like index
-    length_of_input = len(input_string) # Will be used in determining
-    counter = 0 
+    length_of_input = len(input_string) # Will be used in determining how many characters are in the input
+    counter = 0 # Will also be used to track how far from the right is the current iteration.
+
     for characters in input_string[::-1]: # Loops through the characters in the input_text.
         counter += 1 # Will be used to keep track of the index
         if characters != " ":  # If character is not a whitespace
