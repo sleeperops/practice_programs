d9de1d75fae883e61813e92ca6b1acd70d6b19ae
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-02 22:54:53 +0800

    Created functionality

--- replicating_string_methods_2/replicate_upper.py
+++ replicating_string_methods_2/replicate_upper.py
@@ -1,6 +1,11 @@
 # Prompt the user to enter an input
-# Apply lower() to lowercase the entire input
-# Apply swapcase() to reverse the casing of the input
-# Print the result
+input_text = input("Enter a text to be capitalized: ")
 
- 
+def mock_upper(text):
+    text = str(text)
+    text = text.lower() # Apply lower() to lowercase the entire input
+    text = text.swapcase() # Apply swapcase() to reverse the casing of the input
+    return text
+
+# Print the result
+print(mock_upper(input_text))
