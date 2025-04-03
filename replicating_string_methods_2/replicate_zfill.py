1243aa0366c7b219032ef3b07f170880ac9f4033
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-03 21:53:15 +0800

    Move: 3 files moved into replicating_string_methods_2/

--- replicating_string_methods_2/replicate_zfill.py
+++ replicating_string_methods_2/replicate_zfill.py
@@ -1,13 +1,13 @@
 # Prompt the user to enter a sample text/ number
-# Prompt the user to enter the target length 
-# To determine the ammount of zeroes to be used, subtract the length of the sample text to the target length
-# Print the zeroes along with the text
- 
 input_text = input("Enter a text or a number: ")
+
+# Prompt the user to enter the target length 
 target_length = input("Enter the target length of the text: ")
 
+# Subtract the length of the sample text to the target length
 def mock_zfill(text, target_length):
-    zero_ammount = int(target_length) - len(text)
+    zero_ammount = int(target_length) - len(text) 
     return f"{'0' * zero_ammount}{text}"
 
+# Print the zeroes along with the text
 print(mock_zfill(input_text, target_length))
