982352c1d44e20cd79236160b6521d8e8a907c1d
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-03 23:23:23 +0800

    Add file and pseudocode

---
+++ replicating_string_methods_2/replicate_counter.py
@@ -0,0 +1,6 @@
+# Prompt user to enter text
+# Prompt the user to enter the substring to be looked for
+# Split the string into words and store it in a list
+# Loop through the list and use the 'in' operator to search each word for the substring
+# Increment a counter by one for every time the substring is found
+# Print the counter
