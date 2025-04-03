480ce88b3aa57a7d0892c1cb23aa69159284189e
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-03 14:30:34 +0800

    Created replicate_rindex.py and it's pseudocode

---
+++ replicate_rindex.py
@@ -0,0 +1,4 @@
+# Finds the index of the last occurrence of a substring in a string.
+# It takes in a string and its substring as parameters.
+# If the substring does not exist within the string, return 'Substring not detected'.
+# Otherwise return it's index.
