158ee8718e260b4aceb8bc1449a91a2db996aad5
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-03 21:40:25 +0800

    Add suffix removal function

--- replicating_string_methods_2/replicate_removesuffix.py
+++ replicating_string_methods_2/replicate_removesuffix.py
@@ -1,4 +1,25 @@
 # Prompt the user to enter a text 
-# Apply endswith() method to check if the string ends with that suffix
-# If it does, subtract the length of the suffix from the length of the text to get the index of the suffix.
-# return the text and stop at the index of the suffix 
+input_text = input("Enter a text: ")
+
+# Prompt the user to enter a text 
+input_suffix = input("Enter the suffix to be removed: ")
+
+def mock_removesuffix(text, suffix):
+    """
+    Detects and removes the suffix of the text.
+    Takes in the text and the suffix as parameters.
+    If suffix is detected return the suffixless text,
+    otherwise, return None.
+
+    """
+    text = str(text)
+    suffix = str(suffix)
+
+    if text.endswith(suffix):
+        suffix_index = len(text) - len(suffix)
+        return text[0:suffix_index]
+    else:
+        return None
+    
+# Print the result
+print(mock_removesuffix(input_text, input_suffix))
