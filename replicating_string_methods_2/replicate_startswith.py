c24cc4e74e8fec232d7acc99e8b6c7e37db11504
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-03 10:09:16 +0800

    Created replicate_startswith.py and its pseudocode

---
+++ replicating_string_methods_2/replicate_startswith.py
@@ -0,0 +1,5 @@
+# Prompt the user to enter an input text
+# Prompt the user to enter a prefix to check
+# Loop through the prefix and for every character, check if it matches the ones in the input_text.
+# If the loop finishes with all characters matching, return TRUE
+# Otherwise, return FALSE 
