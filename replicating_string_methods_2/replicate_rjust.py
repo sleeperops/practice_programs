89d950f1eeeb9c1b9191cebe311067438d7ee9ad
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-03 11:43:57 +0800

    Renamed directory

--- replicating_string_methods_2/replicate_rjust.py
+++ replicating_string_methods_2/replicate_rjust.py
@@ -1,4 +1,16 @@
 # Prompt user to enter a sample text
-# Prompt user to enter the target length of the text
-# Prompt user to enter the padding that will be used to fill up the spaces
+input_text = input("Enter a text: ")
 
+# Prompt user to enter the target length of the text
+input_target_length = int(input("Enter the target length: "))
+
+# Prompt user to enter the padding that will be used to fill up the spaces
+input_pad = input("Enter the padding that will be used to fill up the spaces: ")
+
+def mock_rjust(text, target_length, pad):
+    text_length = len(text)
+    padding_ammount = target_length - text_length # Determines how much padding will be used to fill the spaces
+    
+    return f"{pad * padding_ammount}{text}"
+
+print(mock_rjust(input_text, input_target_length, input_pad))
