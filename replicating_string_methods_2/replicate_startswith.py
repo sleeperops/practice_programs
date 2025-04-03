7258690604286d35f9e9e4ec8349bb3b4cc3a749
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-03 11:22:30 +0800

    Removed index_mark variable and added error handling

--- replicating_string_methods_2/replicate_startswith.py
+++ replicating_string_methods_2/replicate_startswith.py
@@ -1,24 +1,19 @@
-# Prompt the user to enter an input text
-# Prompt the user to enter a prefix to check
-# Check if the beginning of both text matches
-# If the loop finishes with all characters matching, return TRUE
-# Otherwise, return FALSE 
-
 input_text = input("Enter a text: ")
 prefix = input("Enter the prefix you want to check: ")
 
 def mock_startswith(text, prefix):
     prefix = str(prefix)
     text = str(text)
+
+    if len(prefix) > len(text): # Error handling
+        return False
     
-    index_mark = 0 # Serves to mark the index of the loop 
-    for _ in range(len(prefix)): # The number of loops depends on the length of the prefix
-        if prefix[index_mark] == text[index_mark]:
-            index_mark += 1
+    for index_mark in range(len(prefix)): # The number of loops depends on the length of the prefix
+        if prefix[index_mark] == text[index_mark]: # If the characters in the current index is equal
             continue
-        elif prefix[index_mark] != text[index_mark]:
+        elif prefix[index_mark] != text[index_mark]: # If they do not match
             return False
-    return True
+    return True # If the the program finishes and all character in the prefix matches the start of the text
 
 
 print(mock_startswith(input_text, prefix))
