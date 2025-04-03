64113421ecfdc73d39c780e97ebe136341cfaebd
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-03 21:29:09 +0800

    Created replicate_suffix.py and it's pseudocode

---
+++ replicating_string_methods_2/replicate_removesuffix.py
@@ -0,0 +1,4 @@
+# Prompt the user to enter a text 
+# Apply endswith() method to check if the string ends with that suffix
+# If it does, subtract the length of the suffix from the length of the text to get the index of the suffix.
+# return the text and stop at the index of the suffix 
