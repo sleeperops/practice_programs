0888ee4e42630ce2bbe06be51d455dd90b894013
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-03 13:34:48 +0800

    Created replicate_zfile.py and its pseudocode

---
+++ replicate_zfill.py
@@ -0,0 +1,4 @@
+# Prompt the user to enter a sample text/ number
+# Prompt the user to enter the target length 
+# To determine the ammount of zeroes to be used, subtract the length of the sample text to the target length
+# Print the zeroes along with the text
