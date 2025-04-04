aa70035a3681f726fc72fcaed30bc8b675d8354a
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-04 20:57:33 +0800

    Refactor: Remove test statements and clean up comments

--- replicating_string_methods_2/replicate_counter.py
+++ replicating_string_methods_2/replicate_counter.py
@@ -1,8 +1,8 @@
 # Prompt the user to enter a string and store it in 'text'
-text = "This is a test text this is a test text"
+text = input("Enter a text: ")
 
 # Prompt the user to enter a substring and store it in 'target'
-target = "is"
+target = input("Enter the text you want to look for: ")
 
 # Initialize the variable that temporarily stores the substrings for comparison
 temp_output = ""
@@ -14,30 +14,21 @@ occurrence_count = 0
 index = 0
 
 # Use a while loop and an 'index' to simulate a for loop and loop through the string
-while not index >= len(text): # The loop does not exceed the length of the text
-    for char in text[index:index+len(target)]: # Start from the current index up until nth number of indices
-      print(f"The current index is {index} and it will iterate until {index+len(target)}")
-      temp_output += char # Write each of these characters in 'temp_output'
-    print(f"The current index right now is at {index}")
-    print(f"The current output to be checked is: {temp_output}")
+while index < len(text): 
+    for char in text[index:index+len(target)]:  # Get a slice of text equal to the length of the target, starting at the current index
+      temp_output += char  # Write each of these characters in 'temp_output'
 
     # Check if these characters is equal to the target substring
-    if target == temp_output: # Is equal
-       # If it is, increment 'occurrence_counter' by 1 and move to the next index.
-       print(f"target is equal to temp_output")
-       occurrence_count += 1
-       index += 1 # Move to the next character
-       print(f"Temp_output: {temp_output}")
-    else: # Is not equal
-       # Otherwise, move on and clear 'temp_output'
-       print("target is not equal to tempt output")
-       temp_output = "" # clear the temp_output 
-       index += 1 # Move to the next character
-       print(f"Temp_output = {temp_output}")
+    if target == temp_output:  # If equal
+       occurrence_count += 1  # Increment 'occurrence_counter' by 1 and move on to the next index.
+       index += 1  
+    else:  # If not equal
+       temp_output = ""  # Clear the temp_output 
+       index += 1  # Move to the next character
 
 # Print the 'occurrence_counter'
-print(f" The total count is {occurrence_count}")
-print(f"Count function: {text.count(target)}")
+print(occurrence_count)
+
 
 
 
