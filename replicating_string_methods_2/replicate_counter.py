d8d20177f1fc8d822fd80672a47e7968010c0469
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-04 16:14:51 +0800

    Changed entire program and added pseudocode

--- replicating_string_methods_2/replicate_counter.py
+++ replicating_string_methods_2/replicate_counter.py
@@ -1,21 +1,19 @@
-# Prompt user to enter text
-input_main_string = input("Enter a text: ")
-
-# Prompt the user to enter the substring to be looked for
-input_substring = input("Enter the word to be searched: ")
-
-# Split the string into words and store it in a list
-def mock_counter(main_str,sub_str):
-    word_list = main_str.split()
-    counter = 0
-
-    for words in word_list:
-        if sub_str in words:
-            counter+=1
-    return counter
-    
-# Loop through the list and use the 'in' operator to search each word for the substring
-# Increment a counter by one for every time the substring is found
-# Print the counter
-
-print(mock_counter(input_main_string, input_substring))
+# Prompt the user to enter a string and store it in 'text'
+# Prompt the user to enter a substring and store it in 'target'
+# Use a while loop and an 'index' to simulate a for loop
+# Loop through the string
+# For every item in the string, run another for loop.
+# Start from the current index up until nth number of indices
+# n is equal to the length of the subtring
+# Example: 
+#   string = "abcde", substring = "cde"
+#   iteration 1: "abc"
+#   iteration 2: "bcd"
+#   iteration 3: "cde" <- is equal to substring
+#   iteration 4: "de"
+#   iteration 5: "e"
+# Write each of these characters in 'temp_output'
+# Check if these characters is equal to the target substring
+# If it is, increment 'occurrence_counter' by 1 and move to the next index.
+# Otherwise, move on and clear 'temp_output'
+# Print the 'occurrence_counter'
