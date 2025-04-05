61a5ddfd2fabc9855f97dcdee16aad9bfc452454
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-05 13:40:38 +0800

    Refactor code

--- replicating_string_methods_2/replicate_startswith.py
+++ replicating_string_methods_2/replicate_startswith.py
@@ -5,15 +5,15 @@ def mock_startswith(text, prefix):
     prefix = str(prefix)
     text = str(text)
 
-    if len(prefix) > len(text): # Error handling
+    if len(prefix) > len(text):  # Error handling
         return False
     
-    for index_mark in range(len(prefix)): # The number of loops depends on the length of the prefix
-        if prefix[index_mark] == text[index_mark]: # If the characters in the current index is equal
+    for index_mark in range(len(prefix)):  # The number of loops depends on the length of the prefix
+        if prefix[index_mark] == text[index_mark]:  # If the characters in the current index is equal
             continue
-        elif prefix[index_mark] != text[index_mark]: # If they do not match
+        elif prefix[index_mark] != text[index_mark]:  # If they do not match
             return False
-    return True # If the the program finishes and all character in the prefix matches the start of the text
+    return True  # If the the program finishes and all character in the prefix matches the start of the text
 
 
 print(mock_startswith(input_text, prefix))
