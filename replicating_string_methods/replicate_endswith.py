a8ef8c3fea827e12b54f28b607e963a89ce45786
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-05 13:06:13 +0800

    Fix bug with the loop's range

--- replicating_string_methods/replicate_endswith.py
+++ replicating_string_methods/replicate_endswith.py
@@ -6,18 +6,27 @@ def better_endswith(text, suffix):
     text  = str(text)
     suffix = str(suffix)
 
-    # Determines the index from which the loop will start
+    # Initialize starting index for the main text
     starting_index = len(text) - len(suffix)
+
+    # Initialize starting index for the suffix
+    suffix_index = 0 
 
     # Return False when the length of the suffix is longer than the text
     if len(suffix) > len(text):
         return False
     
-    for index in range(starting_index, len(text) + 1):  
-        if suffix[index] == text[index]:
-            continue  # Character is matchin with each other
-        elif suffix[index] != text[index]:
+    # Cuts a section in the end of the text based on the length of the suffix
+    for index in range(starting_index, len(text)):  # Loop through that section
+        print(f"Current text index: {index}, current suffix index {suffix_index}")
+
+        if suffix[suffix_index] == text[index]:
+            suffix_index += 1  # Increments to move through the suffix by one value
+            print(f"Current characters match")
+            continue  # Character is matching with each other
+        elif suffix[suffix_index] != text[index]:
             return False  # Characters fails to mismatch
     return True  # Loop finishes without any mismatching characters
 
 print(better_endswith(input_text, suffix))
+print(input_text.endswith(suffix))
