6848b9eb48d43287c65eb51d94836e26f3102cff
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-05 12:35:17 +0800

    Revise the function better_endswith()

--- replicating_string_methods/replicate_endswith.py
+++ replicating_string_methods/replicate_endswith.py
@@ -1,24 +1,23 @@
-# If the characters match, move on to the next character in the suffix.
-# Once it fails, break the loop and return FALSE.
-# If the loop finished without discepancies, return TRUE.
-# Call the function and print the result.
-
 # Prompt the user to input a sample_text and a suffix 
 input_text= input("Enter a sample_text: ")
 suffix = input("Enter the suffix: ")
 
-# From right to left, search through the suffix and compare it with the input sample_text
-def better_endswith(input_text, suffix):
-    for suffix_char in suffix[::-1]:
-        for input_char in input_text[::-1]:
-            if suffix_char != input_char:
-                print("FALSE")
-                break
-        else: 
-            print("TRUE")
-            break
+def better_endswith(text, suffix):
+    text  = str(text)
+    suffix = str(suffix)
 
+    # Determines the index from which the loop will start
+    starting_index = len(text) - len(suffix)
 
+    # Return False when the length of the suffix is longer than the text
+    if len(suffix) > len(text):
+        return False
+    
+    for index in range(starting_index, len(text) + 1):  
+        if suffix[index] == text[index]:
+            continue  # Character is matchin with each other
+        elif suffix[index] != text[index]:
+            return False  # Characters fails to mismatch
+    return True  # Loop finishes without any mismatching characters
 
-
-better_endswith(input_text, suffix)
+print(better_endswith(input_text, suffix))
